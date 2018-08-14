97a06b4678ba692ea829404875be515d72db970e
kamihati <dream928@gmail.com>
2018-08-14 08:34:12 +0800

    add process task

--- django/demo1/polls/views.py
+++ django/demo1/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world, You're at the polls index.")
+    return HttpResponse("Hello, world, You're at the polls index.%s" %  datetime.datetime.now())
 
